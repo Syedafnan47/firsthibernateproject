2a71b45a795a34f8fe0881b2e45ac59b241e2372
Syedafnan47 <syedtarooq786@gmail.com>
2025-12-12 11:41:17 +0530

    fourth

--- ThirdHibernateProject/src/main/java/com/kod/ThirdHibernateProject/App.java
+++ ThirdHibernateProject/src/main/java/com/kod/ThirdHibernateProject/App.java
@@ -7,10 +7,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
